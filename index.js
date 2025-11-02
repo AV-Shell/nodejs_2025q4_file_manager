515913e057a426c1c0641364be28d90bec25560f
AV-Shell <avshell.sj@gmail.com>
2025-11-03 00:33:12 +0300

    feat: add task solution

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(process.argv)
+import * as readline from "node:readline/promises";
+import { MyController } from "./common/controller.js";
+
+const controller = new MyController();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("SIGINT", controller.exit);
+rl.on("close", controller.exit);
+rl.on("line", controller.process);
